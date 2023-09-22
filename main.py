1aa9bc70eb6ee79288dd5e68c2864b71fa6637f0
zhangc <zhangc@educ.college-montreal.qc.ca>
2023-09-22 10:32:31 -0400

    Tp1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-chaine = str(input('Entrer une chaine de caracteres:'))
+chaine = str(input('Entrer une chaine de mots:'))
 def count_word(str):
     nombre = len(chaine.split(" "))
     return nombre
