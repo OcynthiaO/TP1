2ee5906554ba4681aaca4a5e732f00bc781870e3
zhangc <zhangc@educ.college-montreal.qc.ca>
2023-09-22 10:29:08 -0400

    Tp1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 chaine = str(input('Entrer une chaine de caracteres:'))
 def count_word(str):
-
-    nombre = len(str)
+    nombre = len(chaine.split(" "))
     return nombre
 
-print("nombre de caracteres : " + '' + str(count_word(chaine)))
+print("nombre de mot : " + '' + str(count_word(chaine)))
